b193c726b7b45721552e711594b5103c51fda0dd
herrBez <mirko.bez@studenti.unipd.it>
2017-06-15 17:02:42 +0200

    Improved description and fixed functions

--- bezplots.R
+++ bezplots.R
@@ -1,21 +1,20 @@
+#Crea un boxplot per ogni qualitativa data una variabile quantitativa y in input
+#Esempio f(data, 'budget'): importante usare gli apici!!!
 f = function(data, y, cn = 2) {
 	a = names(data[ , sapply(data, is.factor), drop=FALSE])
 	l = ceiling(length(a)/cn)
-	print(paste(l, " ", cn))
-	cn=min(cn, l)
+	if(l == 1){
+		cn = 1
+	}
 	par(mfrow = c(l, cn))
 	for ( i in a) {
 		boxplot(data[ , y] ~ data[ , i], ylab=paste(y), xlab = paste(i), main = paste(y, ' vs ', i))
 	}
 }
 
-f(data, 'budget')
-
-
-
-
+#Chiama f su tutte le variabili quantitative
 g = function(dataset, cn = 2){
-	for (k in names(dataset[ , sapply(dataset, is.numeric)])) {
+	for (k in names(dataset[ , sapply(dataset, is.numeric), drop=FALSE])) {
 		f(dataset, k, cn)
 		readline(prompt="Press [enter] to continue")
 	}

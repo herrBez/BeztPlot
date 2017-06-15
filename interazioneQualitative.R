97feac5c91ef2f5d31854f6914fd1c917cad6d35
Matteo Di Pirro <matteodipirro@gmail.com>
2017-06-15 17:55:25 +0200

    Create interazioneQualitative.R

---
+++ interazioneQualitative.R
@@ -0,0 +1,14 @@
+# Crea i boxplot di interazione per tutte le variabili qualitative (da usare nel caso di y qualitativa)
+boxplotQualitative = function(data, cn, x, y) {
+    a = names(data[ , sapply(data, is.factor), drop=FALSE]);
+    l = ceiling(length(a)/cn);
+    if(l == 1){
+        cn = 1
+    }
+    par(mfrow = c(l - 1, cn))
+    for (i in a) {
+        if (i != y) {
+            boxplot(data[ , x] ~ data[ , i] * data[, y], ylab=paste(y), xlab = paste(i), main = paste('Interazione ' , x, ' e ', i))
+        }
+    }
+}

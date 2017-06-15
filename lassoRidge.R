675a20d7d4729bb1fe2fe026aeb37e7d6055eaf6
Davide Polonio <poloniodavide@gmail.com>
2017-06-15 15:40:40 +0200

    Fix

--- lassoRidge.R
+++ lassoRidge.R
@@ -3,16 +3,16 @@
 lassoRidge = function (X, y, tipo, seed=2004) {
     par(mfrow=c(1,2))
     m.lr = glmnet(X, y, alpha=tipo)
-    plot(m.lasso, xvar='lambda', xlab = expression(log(y)))
     set.seed(seed)
     cv.lr = cv.glmnet(X, y, alpha=tipo)
     best.lambda.lr = cv.lr$lambda.min
     best.lambda.1se = cv.lr$lambda.1se
+    modello.min = glmnet(X, y, alpha=tipo, lambda=best.lambda.lr)
+    modello.1se = glmnet(X, y, alpha=tipo, lambda=best.lambda.1se)
+	plot(m.lr, xvar='lambda', xlab = expression(log(y)))
     abline(v=log(best.lambda.lr), lty=2, col='red')
     abline(v=log(best.lambda.1se), lty=2, col='blue')
     plot(cv.lr)
-    modello.min = glmnet(X, y, alpha=tipo, lambda=best.lambda.lr)
-    modello.1se = glmnet(X, y, alpha=tipo, lambda=best.lambda.1se)
     
     return (list ("min" = modello.min, "onese" = modello.1se))
 }
